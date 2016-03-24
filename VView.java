f14faa35e51cf1bf7eefd700ba7b6050629c6825
sahithikhandavalli <sahithikhandavalli@gmail.com>
2016-03-24 19:17:15 +0530

    Added files via upload

---
+++ VView.java
@@ -0,0 +1,22 @@
+package pro.diary.dell.diary;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import pro.diary.dell.diary.R;
+
+public class VView extends AppCompatActivity {
+    ImageView imageView;
+    public Camera1 p;
+    Camera1 path;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vview);
+
+        p=path;
+
+        imageView.setImageDrawable(Drawable.createFromPath(String.valueOf(p)));
+    }}
